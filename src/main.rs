e383084078c5754868ece84a7764cff8ed43456a
VictorBetsch <vic.betsch@gmail.com>
2022-10-04 16:15:10 +0200

    ✨ Added status trigger

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,31 @@
 use std::{
-    io::{prelude::*, BufReader},
-    net::{TcpListener, TcpStream},
+	io::{prelude::*, BufReader},
+	net::{TcpListener, TcpStream},
 };
 
 fn main() {
-    let ping_listen_port = "8000";
+	let ping_listen_port = "8000";
 
-    for stream in TcpListener::bind(["127.0.0.1", ping_listen_port].join(":")).unwrap().incoming() {
-        let _stream = stream.unwrap();
+	for stream in TcpListener::bind(["127.0.0.1", ping_listen_port].join(":")).unwrap().incoming() {
+		let _stream = stream.unwrap();
 
-        handle_connection(_stream);
-    }
+		handle_connection(_stream);
+	}
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+	let buf_reader = BufReader::new(&mut stream);
+	let http_request: Vec<_> = buf_reader
+		.lines()
+		.map(|result| result.unwrap())
+		.take_while(|line| !line.is_empty())
+		.collect();
 
-    // println!("Request: {:#?}", http_request);
-    println!("{}", http_request[0]);
+	let splited = http_request[0].split_whitespace().collect::<Vec<&str>>()[1];
+    
+	if splited == "/ping" {
+		println!("{}", "HTTP GET 202");
+	} else {
+		println!("Error 404")
+	}
 }
